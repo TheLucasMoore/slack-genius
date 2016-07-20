21ed21a866c9f672b0363e8551d56756245159dc
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-07-19 20:45:43 -0600

    formatting of URL, otherwise works

--- index.js
+++ index.js
@@ -29,12 +29,7 @@ app.post('/spotify', function(req, res){
     
       var body = {
       response_type: "in_channel",
-      text: bio[0],
-      attachments: [
-          {
-          text: spotlink
-          }
-        ]
+      text: bio[0] \nspotlink,
       };
       res.send(body)
       })
