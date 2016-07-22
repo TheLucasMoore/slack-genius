9c2d7e934fb8b940e9ca9af85c30dc06e820a813
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-07-22 12:41:28 -0600

    error handling

--- index.js
+++ index.js
@@ -52,20 +52,24 @@ app.post('/album', function(req, res){
       var albumName = data.albums.items[0].name
       var albumLink = data.albums.items[0].external_urls.spotify
       var albumArt = data.albums.items[0].images[0].url
-    }
-    var body = {
-      "response_type": "in_channel",
-      "text": albumLink,
-      "attachments": [
-      {
-        "title": albumName,
-        "title_link": albumLink,
-        "image_url": albumArt
-      }
-      ]
-    };
+
+      var body = {
+        "response_type": "in_channel",
+        "text": albumLink,
+        "attachments": [
+        {
+          "title": albumName,
+          "image_url": albumArt
+        }
+        ]
+        };
+    } else {
+      var body = {
+        response_type: "in_channel",
+        text: "There was an error! " + error
+      };
     res.send(body);
-  });
+  };
 });
 
 app.post('/genius', function(req, res){
